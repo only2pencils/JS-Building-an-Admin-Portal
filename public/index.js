84514bd67b09074f5dc8a2cd8bb0f6a9355c384d
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-07-20 19:17:20 -0400

    1st commit

--- public/index.js
+++ public/index.js
@@ -1,28 +1,34 @@
 async function main() {
+  let response = await fetch("http://localhost:9001/getBooks");
+  let books = await response.json();
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
-
-    books.forEach(renderBook)
+  books.forEach(renderBook);
 }
 
 function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML += `
-        <div class="col-sm-3">
-            <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
-                    <img class="card-img-top" src="${book.imageURL}" />
+  let bookContainer = document.querySelector(".book-container");
+  bookContainer.innerHTML += `
+            <div class="ui card">
+                ${
+                  book.imageURL
+                    ? `
+                    <div class="image">
+                        <img src="${book.imageURL}" />
+                    </div> 
                 `
-                : ``}
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
-                    <p class="card-text">${book.description}</p>
+                    : ``
+                }
+                <div class="content">
+                    <a class="header">${book.title}</a>
+                    <div class="meta">
+                        <span class="date">Available: ${book.quantity}</span>
+                </div>
+                <div class="description">
+                     ${book.description}
                 </div>
             </div>
         </div>
-    `
+    `;
 }
 
-main()
+main();
